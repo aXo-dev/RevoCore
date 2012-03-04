69801c760e9653c0cf03462b8634bc0d9b40fd77
aXo Dev <axo.github@gmail.com>
2012-03-04 13:40:09 -0500

    edited cmake options, fixed revision information

--- cmake/showoptions.cmake
+++ cmake/showoptions.cmake
@@ -1,9 +1,9 @@
 # output generic information about the core and buildtype chosen
 message("")
-message("* TrinityCore rev. hash  : ${rev_hash}")
-message("* TrinityCore rev. date  : ${rev_date}")
+message("* RevoCore rev. hash  : ${rev_hash}")
+message("* RevoCore rev. date  : ${rev_date}")
 if( UNIX )
-  message("* TrinityCore buildtype  : ${CMAKE_BUILD_TYPE}")
+  message("* RevoCore buildtype  : ${CMAKE_BUILD_TYPE}")
 endif()
 message("")
 
